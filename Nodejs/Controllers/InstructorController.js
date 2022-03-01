ca6e210fb3f66937837ce9d9a80311a84c684b46
RandaHadad <randahada99@gmail.com>
2022-03-01 18:50:04 +0200

    finalllll

--- Nodejs/Controllers/InstructorController.js
+++ Nodejs/Controllers/InstructorController.js
@@ -4,24 +4,49 @@ const InstructorInfo = require("../Models/InstructorInfo");
 exports.route = function(req, res) {
     let user = req.session.user;
     InstructorInfo.getInstructor(user.user_id, function(instructorinfo) {
-        // let studentDegree=[]
-        // for (let i = 0; i < instructorinfo; i++) {
-        //     getStudentDegree.getDegree(instructorinfo[i].course_id, function(studentdeg) {
+        let stddrgree = [];
+        if (instructorinfo.length == 1) {
+            getStudentDegree.getDegree(instructorinfo[0].course_id, function(studentdeg) {
+                stddrgree.push({
+                    coursename: instructorinfo[0].course_name,
+                    student: studentdeg
+                })
+                if (user) {
+                    res.render('Instructor', {
+                        id: instructorinfo[0].ins_id,
+                        username: user.user_name,
+                        address: instructorinfo[0].ins_address,
+                        department: instructorinfo[0].dept_description,
+                        coursename: instructorinfo[0].course_name,
+                        students: stddrgree
+                    })
+                }
+                console.log(studentdeg, instructorinfo[0])
+            })
+        } else {
+            getStudentDegree.getDegree(instructorinfo[0].course_id, function(course1) {
+                getStudentDegree.getDegree(instructorinfo[1].course_id, function(course2) {
+                    stddrgree = [{
+                            coursename: instructorinfo[0].course_name,
+                            student: course1
+                        },
+                        {
+                            coursename: instructorinfo[1].course_name,
+                            student: course2
+                        }
+                    ]
 
-        //     })
-        // }
-
-        if (user) {
-            console.log(instructorinfo[0])
-            res.render('Instructor', {
-                id: instructorinfo[0].ins_id,
-                username: user.user_name,
-                address: instructorinfo[0].ins_address,
-                department: instructorinfo[0].dept_description
-
+                    if (user) {
+                        res.render('Instructor', {
+                            id: instructorinfo[0].ins_id,
+                            username: user.user_name,
+                            address: instructorinfo[0].ins_address,
+                            department: instructorinfo[0].dept_description,
+                            students: stddrgree
+                        })
+                    }
+                })
             })
         }
-
     })
-
 }
